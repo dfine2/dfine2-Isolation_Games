4e0fac39ad22288b3ac8ad5e7a796870e06b8e7a
David Fine <dfine39@gmail.com>
2020-03-28 17:08:45 -0400

    Updates

---
+++ precache-manifest.985fcc0f77aafc8fd7e4ac3c8886bd40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397ca05dac659622169770d7099ee03e",
+    "url": "/dfine2-Isolation_Games/index.html"
+  },
+  {
+    "revision": "c10f9b014a22521d1e4b",
+    "url": "/dfine2-Isolation_Games/static/js/2.b0b3d75c.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/dfine2-Isolation_Games/static/js/2.b0b3d75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e720d2db821adbea1c5",
+    "url": "/dfine2-Isolation_Games/static/js/main.14121eba.chunk.js"
+  },
+  {
+    "revision": "d342ed1d869a0e542213",
+    "url": "/dfine2-Isolation_Games/static/js/runtime-main.6df5c877.js"
+  }
+]);
