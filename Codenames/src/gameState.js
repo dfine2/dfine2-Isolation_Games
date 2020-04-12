6084acad73cba6d00e5c3066d767c374e624d2b7
David Fine <dfine39@gmail.com>
2020-04-12 14:28:40 -0400

    switch ngrok

--- Codenames/src/gameState.js
+++ Codenames/src/gameState.js
@@ -4,7 +4,7 @@ import React, {createContext, useState, useContext, useEffect} from 'react'
 
 //yarn ngrok http -host-header=rewrite 3000
 
-const endpoint = "https://6dd98359.ngrok.io"
+const endpoint = "http://e41d2841.ngrok.io"
 const socket = openSocket(endpoint)
 
 
